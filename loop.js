b2e69a9b5a685c839d634a6d6c673214148d7300
Athar Bhat <108935066+Atharrj5@users.noreply.github.com>
2025-07-02 11:54:38 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,31 @@
+Arithmetic Operators:
+a + b = 15
+a - b = 5
+a * b = 50
+a / b = 2
+a % b = 0
+
+Comparison & Logical Operators:
+a > b: true
+a < b: false
+a == 10 && b == 5: true
+a == 10 || b == 3: true
+!(a == 10): false
+
+If-Else Condition:
+a is greater than b
+
+Switch Case Example:
+Good
+
+For Loop (1 to 5):
+i = 1
+i = 2
+i = 3
+i = 4
+i = 5
+
+While Loop (Countdown from 3):
+Count = 3
+Count = 2
+Count = 1
